05aedc578a850b9589dfb222b4c4dc74a0b9386c
Araa2876 <anibal.arango@usherbrooke.ca>
2026-03-25 11:22:51 -0400

    Changing node name

--- launch/endpoint.py
+++ launch/endpoint.py
@@ -6,7 +6,7 @@ def generate_launch_description():
     return LaunchDescription(
         [
             Node(
-                package="ros_tcp_endpoint",
+                package="scorpius_tcp",
                 executable="default_server_endpoint",
                 emulate_tty=True,
                 parameters=[{"ROS_IP": "127.0.0.1"}, {"ROS_TCP_PORT": 10000}],
